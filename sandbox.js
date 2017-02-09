d3bf4635d260f49d8d22078225da19095bcb87fa
Meryl Dakin <meryl.k.dakin@gmail.com>
2017-02-09 00:57:51 +0000

    m

--- sandbox.js
+++ sandbox.js
@@ -24,7 +24,7 @@ $(function () {
                     $("<div>")
                     .attr("class", "row filter-row")
                     .attr("id", "phoneRow")
-                    
+                    .appendTo($productSection)
                     // DIV COL Image, 2
                     .append($('<div>')
                         .attr("class", "col-xs-2")
@@ -49,10 +49,9 @@ $(function () {
                             .append($("<div>")
                                 .attr("class", "col-xs-12")
                                 .append($("<button>")
-                                    .attr("id", "modal-button" + index)
-                                    .attr("class", "btn btn-info btn-block glyphicon glyphicon-zoom-in")
-                                    .attr("data-toggle","modal")
-                                    .attr("data-target", "#myModal")
+                                    .attr("id", "modal-button")
+                                    .attr("class", "btn btn-info btn-block")
+                                    .text("Magnify")
                                 
                                 ) // end btn
                             
@@ -82,7 +81,7 @@ $(function () {
                             // DIV COL Price, 6
                             .append($("<div>")
                                 .attr("class", "col-xs-6 h4")
-                                .text("Price: $" + values.price)
+                                .text("Price: " + values.price)
                                 .attr("id", "price-id"+index)
                             )
                             //DIV COL Stock, 6
@@ -103,15 +102,7 @@ $(function () {
                             // DIV COL Spec Details, 10
                             .append($("<div>")
                                 .attr("class", "col-xs-10 specs-details")
-                                .append($("<ul>")
-                                    // .append($("<li>")
-                                        .attr("id", "specs-list"+index)
-                                        .append($("<div>")
-                                            .text(values.specs)
-                                            .wrapInner("<li>"))
-                                    // )
-                                )
-                                
+                                .text(values.specs)
                             )
                         )
                     )
@@ -138,74 +129,14 @@ $(function () {
             });
         
         });
-/***************** MODAL ***********************************************/            
+/***************** END SEARCH BAR***********************************************/            
 
-    let imageArray = _.map(data, function(value){
-        return "img/product/" + value.image;
-    });
-    
-    let phoneTitles = _.map(data, function(value){
-        return value.desc;
-    });
-    
-    let specsText = _.map(data, function(value){
-        return value.specs;
-    });
-    
-    let altColors = _.map(data, function(value){
-        return value.availableColors;
-    });
-    
-    let colorSentence = _.map(altColors, function(value, index) {
-        let sent = []
-        _.each(value, function(val, i){
-            if (value.indexOf(val) === value.length -1){
-                sent.push("and " + val + ".")
-            } else {
-                sent.push(val + ", ");
-            }
-        })
-        return sent.join("");
-    })
-    
-    _.map(phoneTitles, function(value, index){
-        $("#modal-button"+ index).on('click',function(){
-            let title = $('#myModalLabel').text(value);
-            let image = $('#modal-image').attr("src", imageArray[index]).attr("class", "img-responsive center-block");
-            let availColors = $('#alt-colors').text("Also available in " + colorSentence[index]).attr("class", "lead");
-        });
-    });
-    
-    /**************************Specs in bullets********************/
-    // let specsList = _.map(data, function(value){
-    //     return value.specs;
-    // });
-    
-    // let wrapSpecs = _.map(specsList, function(specArr, index){
-    //     return _.each(specArr, function(spec, ind){
-    //         $(spec).wrap("<li>");
-    //     });
-    // });
-    
-    // console.log(wrapSpecs);
-    
-    //DIV COL Stock, 6
-    // .append($("<div>")
-    //     .attr("class", "col-xs-6 h4 stock-number")
-    //     .text("In Stock: " + values.stock)
-    //     .attr("id", "stock-id"+index)
-    // )
 
-    let stockNums = _.map(data, function(value) {
-        return value.stock;
-    })
-    console.log(stockNums);
+        
+        
+        
     
-    let lowStock = _.each(stockNums, function(stockNum, index){
-        if (stockNum < 10){
-            return $("#stock-id"+index).text("Only " + stockNum + " left!").attr("class", "h4 text-danger");
-        }
-    })
+
   
   
     });
